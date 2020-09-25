4b165792efcae00a71ada5e66e125f200e8e57eb
shashi-chandel <71683377+shashi-chandel@users.noreply.github.com>
2020-09-25 17:06:57 +0530

    Player Checks for option

--- snakeladdersimulator/SnakeLadderSimulator.java
+++ snakeladdersimulator/SnakeLadderSimulator.java
@@ -3,8 +3,23 @@ package com.snakeladdersimulator;
 public class SnakeLadderSimulator {
 	public static void main(String[] args) {
 	int position = 0;
-	System.out.println("Position of the Single player: " + position);
 	int roll = (int)(Math.random()*(6-1))+1;
 	System.out.println("Number after rolling die: " + roll);
+	int turn = (int)(Math.random() * 10 ) % 3;
+	switch (turn) {
+	case 1:
+		System.out.println("Ladder!!");
+		position +=roll;
+		break;
+	case 2:
+		System.out.println("Snake!!");
+		position -=roll;
+		if(position<0)
+			position = 0;
+		break;
+	default:
+		System.out.println("No Play!!");
+	}
+	System.out.println("Position of the Single player: " + position);
 	}
 }
