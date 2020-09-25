ea3500537abb40b57e6182dab204a6cd94ed6251
shashi-chandel <71683377+shashi-chandel@users.noreply.github.com>
2020-09-25 16:55:39 +0530

    Player rolls die

--- snakeladdersimulator/SnakeLadderSimulator.java
+++ snakeladdersimulator/SnakeLadderSimulator.java
@@ -2,6 +2,9 @@ package com.snakeladdersimulator;
 
 public class SnakeLadderSimulator {
 	public static void main(String[] args) {
-	System.out.println("Welcome to Snake & Ladder Game!!");
-}
+	int position = 0;
+	System.out.println("Position of the Single player: " + position);
+	int roll = (int)(Math.random()*(6-1))+1;
+	System.out.println("Number after rolling die: " + roll);
+	}
 }
