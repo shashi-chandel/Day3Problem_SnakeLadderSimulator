30be1c3fec3cfef93617aa6b5d24e91e96251f1d
shashi-chandel <71683377+shashi-chandel@users.noreply.github.com>
2020-09-25 19:28:32 +0530

    Two Players Game with a winner

--- snakeladdersimulator/SnakeLadderSimulator.java
+++ snakeladdersimulator/SnakeLadderSimulator.java
@@ -2,33 +2,110 @@ package com.snakeladdersimulator;
 
 public class SnakeLadderSimulator {
 	public static void main(String[] args) {
-	int position = 0, role=0;
-	while(position<100) 
+	int position1 = 0, position2 = 0, role1=0, role2=0;
+	while(position1<100 && position2<100) 
 	{
-	int roll = (int)(Math.random()*(6-1))+1;
-	System.out.println("\nNumber after rolling die: " + roll);
-	int turn = (int)(Math.random() * 10 ) % 3;
-	switch (turn) {
+	int roll1 = (int)(Math.random()*(6-1))+1;
+	int roll2 = (int)(Math.random()*(6-1))+1;
+	//System.out.println("\nNumber after rolling die: " + roll);
+	int turn1 = (int)(Math.random() * 10 ) % 3;
+	int turn2 = (int)(Math.random() * 10 ) % 3;
+	switch (turn1) 
+	{
 	case 1:
 		System.out.println("Ladder!!");
-		if(!((position+roll)>100))
-			position +=roll;
+		if(!((position1+roll1)>100))
+			{ position1 += roll1;
+						while(turn1 == 1) 
+						{
+								turn1 = (int)(Math.random() * 10 ) % 3;
+								switch (turn1) 
+								{
+								case 1:
+									System.out.println("Ladder!!");
+									if(!((position1+roll1)>100))
+										position1 +=roll1;
+									else
+										System.out.println("No turn");
+									break;
+								case 2:
+									System.out.println("Snake!!");
+									position1 -=roll1;
+									if(position1<0)
+										position1 = 0;
+									break;
+								default:
+									System.out.println("No Play!!");
+								}
+						}
+			}
 		else
 			System.out.println("No turn");
 		break;
 	case 2:
 		System.out.println("Snake!!");
-		position -=roll;
-		if(position<0)
-			position = 0;
+		position1 -=roll1;
+		if(position1<0)
+			position1 = 0;
 		break;
 	default:
 		System.out.println("No Play!!");
 			}
-	role++;
-	System.out.println("Position of the player: " + position);
+	role1++;
+	System.out.println("Position of the player1: " + position1);
+	
+	
+	switch (turn2) {
+	case 1:
+		System.out.println("Ladder!!");
+		if(!((position2+roll2)>100))
+			{position2 +=roll2;
+							while(turn2 == 1) 
+							{
+									turn2 = (int)(Math.random() * 10 ) % 3;
+									switch (turn2) {
+									case 1:
+										System.out.println("Ladder!!");
+										if(!((position2+roll2)>100))
+											position2 +=roll2;
+										else
+											System.out.println("No turn");
+										break;
+									case 2:
+										System.out.println("Snake!!");
+										position2 -=roll2;
+										if(position2<0)
+											position2 = 0;
+										break;
+									default:
+										System.out.println("No Play!!");
+											}
+							}
+			}
+		else
+			System.out.println("No turn");
+		break;
+	case 2:
+		System.out.println("Snake!!");
+		position2 -=roll2;
+		if(position2<0)
+			position2 = 0;
+		break;
+	default:
+		System.out.println("No Play!!");
+			}
+	role2++;
+	System.out.println("Position of the player2: " + position2);
 	}
-	System.out.println("\n        Player wins the game!!!");
-	System.out.println(" \n Number of roles to win: " + role);
+	if (position1<position2)
+	{
+	System.out.println("\n        Player2 wins the game!!!");
+	System.out.println(" \n Number of roles to win: " + role2);
 	}
+	else 
+	{
+		System.out.println("\n        Player1 wins the game!!!");
+		System.out.println(" \n Number of roles to win: " + role1);
+	}
+}
 }
